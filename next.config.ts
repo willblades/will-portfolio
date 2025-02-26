e8dd7af2362bfcd64a3c92ac14d1139c379716ab
Will Blades <will.blades@gmail.com>
2025-02-25 16:17:36 -0800

    enabled export in nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: "export",
+  output: "export",
  
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
